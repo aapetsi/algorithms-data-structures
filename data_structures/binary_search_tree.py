e9942d0d257a2d00b4e38659c38981096c5ac699
aapetsi <apetsiampiah@gmail.com>
2020-03-16 06:18:26 +0000

    add find method to BST

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -31,8 +31,39 @@ class BinarySearchTree:
                     return self
                 current = current.right
 
+    def find(self, value):
+        if self.root is None:
+            return False
+        current = self.root
+        found = False
+        while not found and current:
+            if value < current.value:
+                current = current.left
+            elif value > current.value:
+                current = current.right
+            else:
+                found = True
+        if not found:
+            return False
+        return current
+    
+    def contains(self, value):
+        if self.root is None:
+            return False
+        current = self.root
+        found = False
+        while not found and current:
+            if value < current.value:
+                current = current.left
+            elif value > current.value:
+                current = current.right
+            else:
+                return True
+        return False
+
 
 tree = BinarySearchTree()
+# print(tree.find(1))
 tree.insert(10)
 tree.insert(5)
 tree.insert(13)
@@ -40,4 +71,6 @@ tree.insert(11)
 tree.insert(2)
 tree.insert(16)
 tree.insert(7)
-print(tree.root.right.value)
+print(tree.find(10))
+print(tree.contains(10))
+# print(tree.root.right.value)
