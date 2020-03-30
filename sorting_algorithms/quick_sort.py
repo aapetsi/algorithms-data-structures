fdc399cad1c517caa653e5bcd33e29786dba484e
aapetsi <apetsiampiah@gmail.com>
2020-03-30 10:03:03 +0000

    fix linting in quick sort

--- sorting_algorithms/quick_sort.py
+++ sorting_algorithms/quick_sort.py
@@ -1,11 +1,9 @@
 def quick_sort(arr):
     if len(arr) < 2:
         return arr
-    
+
     pivot = arr[0]
-    lesser = [i  for i in arr[1:] if i <= pivot]
+    lesser = [i for i in arr[1:] if i <= pivot]
     greater = [i for i in arr[1:] if i > pivot]
-    print(lesser, greater)
+
     return quick_sort(lesser) + [pivot] + quick_sort(greater)
-    
-print(quick_sort([10,4,4,5,9,22]))
